02b6e7d1c4736174fdccde1b83f36c8643189f28
dskst <10830352+dskst@users.noreply.github.com>
2024-09-02 11:55:21 +0900

    feat: first commit

---
+++ src/command.js
@@ -0,0 +1,14 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const DEPLOY4PAGE_COMMAND = {
+    name: 'deploy4page',
+    description: 'Deploy cloudflare pages.',
+};
+
+export const INVITE_COMMAND = {
+    name: 'invite',
+    description: 'Get an invite link to add the bot to your server',
+};
